12eaa86727f3133b8d41145c5dc56bc93a1f96f2
athamour1 <thanosmour.tk@gmail.com>
2023-05-23 19:03:10 +0300

    Implement: open jira ticket functionality

--- index.js
+++ index.js
@@ -1,16 +1,63 @@
 require('dotenv').config();
 
 const { InfluxDB } = require('@influxdata/influxdb-client');
+const axios = require('axios');
+
+const queries = require('./queries.json');
 
 /** Environment variables **/
 const url = process.env.INFLUX_URL || '';
 const token = process.env.INFLUX_TOKEN;
 const org = process.env.INFLUX_ORG || '';
 
+const jiraSiteUrl = process.env.JIRA_SITE_URL || '';
+const jiraSiteAuth = process.env.JIRA_SITE_AUTH || '';
+const jiraSiteProject = process.env.JIRA_SITE_PROJECT || '';
+const jiraSiteIssuetype = process.env.JIRA_ISSUE_TYPE || '';
+
+
 const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
 
-const queries = require('./queries.json');
+const createJiraTicket = async (device, lastTime) => {
+    try {
+        const url = jiraSiteUrl;
+        const auth = 'Basic ' + jiraSiteAuth;
 
+        const payload = {
+            fields: {
+                summary: device + 'has 12 hours to report a value',
+                issuetype: { id: jiraSiteIssuetype },
+                project: { key: jiraSiteProject },
+                description: {
+                    type: 'doc',
+                    version: 1,
+                    content: [
+                        {
+                            type: 'paragraph',
+                            content: [
+                                {
+                                    text: device + 'has 12 hours to report a value. Last time was' + lastTime,
+                                    type: 'text',
+                                },
+                            ],
+                        },
+                    ],
+                },
+            },
+        };
+
+        const headers = {
+            Authorization: auth,
+            'Content-Type': 'application/json',
+            Accept: 'application/json',
+        };
+
+        const response = await axios.post(url, payload, { headers });
+        console.log('Ticket created successfully:', response.data);
+    } catch (error) {
+        console.error('Error creating ticket:', error.response.data);
+    }
+};
 
 const getLastValue = async (query) => {
     let lastValue = null; // Variable to store the last value
@@ -32,9 +79,10 @@ const executeQueries = async () => {
                 const lastValueTimestamp = new Date(Date.parse(lastValue));
 
                 if (lastValueTimestamp < twelveHoursAgo) {
-                    console.log(name + ': Last value is older than 10 secs.');
+                    createJiraTicket(name, lastValue);
+                    console.log(name + ': Last value is older than 12 Hours.');
                 } else {
-                    console.log(name + ': Last Value:', lastValue); // Print the last value
+                    console.log(name + ': Last Time reported:', lastValue); // Print the last value
                 }
             })
             .catch((error) => {
