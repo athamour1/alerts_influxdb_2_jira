45febd306742e76b66f25b1b0da882614277732b
athamour1 <thanosmour.tk@gmail.com>
2023-05-23 18:51:09 +0300

    Refactor: index.js

--- index.js
+++ index.js
@@ -11,18 +11,6 @@ const queryApi = new InfluxDB({ url, token }).getQueryApi(org);
 
 const queries = require('./queries.json');
 
-// const queries = [
-//     {
-//         name: 'query1',
-//         query: `from(bucket: "home_assistant") |> range(start: -12h)  // Retrieve data from the last 12 hours |> filter(fn: (r) => r["_measurement"] == "°C") |> filter(fn: (r) => r["_field"] == "value") |> filter(fn: (r) => r["domain"] == "sensor") |> filter(fn: (r) => r["entity_id"] == "pm2_5_monitor_zwave_plus_air_temperature") |> aggregateWindow(every: 1h, fn: mean, createEmpty: false) |> yield(name: "mean")`,
-//     },
-//     {
-//         name: 'query2',
-//         query: `from(bucket: "home_assistant") |> range(start: -12h)  // Retrieve data from the last 12 hours |> filter(fn: (r) => r["_measurement"] == "µg/m³") |> filter(fn: (r) => r["_field"] == "value") |> filter(fn: (r) => r["domain"] == "sensor") |> filter(fn: (r) => r["entity_id"] == "pm2_5_monitor_zwave_plus_particulate_matter_2_5") |> aggregateWindow(every: 1h, fn: mean, createEmpty: false) |> yield(name: "mean")`,
-//     },
-//     // Add more queries as needed
-// ];
-
 
 const getLastValue = async (query) => {
     let lastValue = null; // Variable to store the last value
@@ -42,9 +30,6 @@ const executeQueries = async () => {
                 const now = new Date();
                 const twelveHoursAgo = new Date(now.getTime() - 12 * 60 * 60 * 1000);
                 const lastValueTimestamp = new Date(Date.parse(lastValue));
-
-                // console.log(twelveHoursAgo);
-                // console.log(lastValueTimestamp);
 
                 if (lastValueTimestamp < twelveHoursAgo) {
                     console.log(name + ': Last value is older than 10 secs.');
